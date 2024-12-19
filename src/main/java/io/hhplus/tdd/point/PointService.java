b77e62cd127883e529652c5773f15494d4288cc7
ehoh <oheunhye95@gmail.com>
2024-12-20 03:03:51 +0900

    Revert "Service 동시성 제어 처리"
    This reverts commit b2df1490b7dd6ed10b9e06798e908673a2533835.

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.locks.ReentrantLock;
 
 @Service
 public class PointService {
@@ -19,20 +17,6 @@ public class PointService {
     PointHistoryTable pointHistoryTable;
 
     private final long MAX_POINT = 10000L;
-
-    /**
-     * 각 ID에 대해 ReentrantLock을 저장하는 맵
-     * ID마다 별도의 락을 관리하는 용도
-     * */
-    private final ConcurrentHashMap<Long, ReentrantLock> locks = new ConcurrentHashMap<>();
-
-    /**
-     * ID별로 lock를 생성하거나 기존 lock를 반환하는 메소드
-     * */
-    private ReentrantLock getLock(Long id) {
-        locks.putIfAbsent(id, new ReentrantLock());
-        return locks.get(id);
-    }
 
 
     /**
@@ -48,7 +32,7 @@ public class PointService {
         return userPoint;
     }
 
-
+    
     /**
      * id로 PointHistory 조회해 List로 반환
      * 예외 케이스 1.조회 값이 null
@@ -58,7 +42,7 @@ public class PointService {
         if(listPointHistory == null) {
             throw new NullPointerException("이력이 없습니다.");
         }
-
+        
         return listPointHistory;
     }
 
@@ -70,34 +54,24 @@ public class PointService {
      * */
     public UserPoint charge(long id, long amount) {
 
-        ReentrantLock lock = getLock(id);
-        lock.lock(); //id에 대해 락을 걸어서 다른 요청이 해당 유저에 처리되지 않도록 함
-
-        try {
-
-            UserPoint existingUserPoint = userPointTable.selectById(id);
-            if(existingUserPoint == null) {
-                throw new IllegalArgumentException("[포인트 충전 실패] 등록된 id가 아닙니다.");
-            }
-
-            long updateAmount = amount + existingUserPoint.point();
-            if(updateAmount > MAX_POINT) {
-                throw new IllegalArgumentException("[포인트 충전 실패] 충전 가능한 최대 포인트를 초과했습니다.");
-            }
-
-            UserPoint updated = userPointTable.insertOrUpdate(id, updateAmount);
-            if(updated == null) {
-                throw new IllegalStateException("[포인트 충전 실패] 유저 포인트가 정상적으로 업데이트되지 않았습니다.");
-            }
-
-            pointHistoryTable.insert(updated.id(), updated.point(), TransactionType.CHARGE, updated.updateMillis());
-
-            return updated;
-
-        } finally {
-            lock.unlock(); //처리 후 락 해제(다른 스레드가 해당 id에 대해 처리할 수 있게 함)
+        UserPoint existingUserPoint = userPointTable.selectById(id);
+        if(existingUserPoint == null) {
+            throw new IllegalArgumentException("[포인트 충전 실패] 등록된 id가 아닙니다.");
         }
 
+        long updateAmount = amount + existingUserPoint.point();
+        if(updateAmount > MAX_POINT) {
+            throw new IllegalArgumentException("[포인트 충전 실패] 충전 가능한 최대 포인트를 초과했습니다.");
+        }
+
+        UserPoint updated = userPointTable.insertOrUpdate(id, updateAmount);
+        if(updated == null) {
+            throw new IllegalStateException("[포인트 충전 실패] 유저 포인트가 정상적으로 업데이트되지 않았습니다.");
+        }
+
+        pointHistoryTable.insert(updated.id(), updated.point(), TransactionType.CHARGE, updated.updateMillis());
+
+        return updated;
     }
 
     /**
@@ -108,33 +82,23 @@ public class PointService {
      * */
     public UserPoint use(long id, long amount) {
 
-        ReentrantLock lock = getLock(id);
-        lock.lock(); //id에 대해 락을 걸어서 다른 요청이 해당 유저에 처리되지 않도록 함
-
-        try {
-
-            UserPoint existingUserPoint = userPointTable.selectById(id);
-            if(existingUserPoint == null) {
-                throw new IllegalArgumentException("[포인트 사용 실패] 등록된 id가 아닙니다.");
-            }
-
-            if(existingUserPoint.point() - amount < 0) {
-                throw new IllegalArgumentException("[포인트 사용 실패] 사용 가능한 포인트가 부족합니다.");
-            }
-            long updateAmount = existingUserPoint.point() - amount;
-
-            UserPoint updated = userPointTable.insertOrUpdate(id, updateAmount);
-            if(updated == null) {
-                throw new IllegalStateException("[포인트 사용 실패] 유저 포인트가 정상적으로 업데이트되지 않았습니다.");
-            }
-
-            pointHistoryTable.insert(updated.id(), updated.point(), TransactionType.USE, updated.updateMillis());
-
-            return updated;
-
-        } finally {
-            lock.unlock();
+        UserPoint existingUserPoint = userPointTable.selectById(id);
+        if(existingUserPoint == null) {
+            throw new IllegalArgumentException("[포인트 사용 실패] 등록된 id가 아닙니다.");
         }
 
+        if(existingUserPoint.point() - amount < 0) {
+            throw new IllegalArgumentException("[포인트 사용 실패] 사용 가능한 포인트가 부족합니다.");
+        }
+        long updateAmount = existingUserPoint.point() - amount;
+
+        UserPoint updated = userPointTable.insertOrUpdate(id, updateAmount);
+        if(updated == null) {
+            throw new IllegalStateException("[포인트 사용 실패] 유저 포인트가 정상적으로 업데이트되지 않았습니다.");
+        }
+
+        pointHistoryTable.insert(updated.id(), updated.point(), TransactionType.USE, updated.updateMillis());
+
+        return updated;
     }
 }
